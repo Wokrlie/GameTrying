d6f0e14d8616c97bbcfc564f4e15a93adc977fa6
Wokrlie <hehelilibz@gmail.com>
2026-03-23 16:36:09 +0800

    update(main): do the basic framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <raylib.h>
+
+struct Properties {
+    int window_default_width;
+    int window_default_height;
+    const char* window_title;
+};
+
+struct Status {};
+
+void Update(Status* status) {}
+void Draw(Status* status) {}
 
 int main() {
-    std::cout << "Hello world!";
+    Properties properties;
+    properties.window_default_width = 1280;
+    properties.window_default_height = 720;
+    properties.window_title = "Game";
+
+    Status status;
+    
+    InitWindow(properties.window_default_width, properties.window_default_height, properties.window_title);
+    while (!WindowShouldClose()) {
+        Update(&status);
+        BeginDrawing();
+        Draw(&status);
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
