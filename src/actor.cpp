b79cdb2a7a3487c886271a8ee4787050b9a50aaa
Wokrlie <hehelilibz@gmail.com>
2026-04-19 14:01:42 +0800

    fix(actor): add missing `Init` function.

--- src/actor.cpp
+++ src/actor.cpp
@@ -1,10 +1,27 @@
 #include "actor.h"
 
-Actor::Actor(Vector2 pos, Vector2 size) :m_pos(pos), m_rect({pos.x, pos.y, size.x, size.y}) {}
+Actor::Actor(Vector2 pos, Vector2 size) :m_pos(pos), m_rect({pos.x, pos.y, size.x, size.y}) { Init(); }
 
-Rectangle Actor::GetRect() { return m_rect; }
+void Actor::Init() {}
 
-void Actor::Update(float dt) {}
+void Actor::AddNewPlugin(std::unique_ptr<Plugin> p) {
+    m_plugins.push_back(std::move(comp));
+}
+
+void Actor::Update(float dt) {
+    for ( auto& plugin : m_plugins ) {
+        plugin.Update(dt);
+    }
+}
+
 void Actor::Draw() {
     DrawRectangle(m_pos.x, m_pos.y, m_rect.width, m_rect.height, GREEN);
 }
+
+void Actor::Shutdown() {
+    for ( auto& plugin : m_plugins ) {
+        plugin.shutdown();
+    }
+}
+
+Rectangle Actor::GetRect() { return m_rect; }
--- src/actor.h
+++ src/actor.h
@@ -1,25 +1,29 @@
 #pragma once
+#include <vector>
+#include <memory>
+
 #include <raylib.h>
 #include <box2d/box2d.h>
+
+#include "plugin.h"
 
 class Actor {
     public:
         Actor(Vector2 pos, Vector2 size);
+        void Init();
         virtual void Update(float dt);
         virtual void Draw();
+        void Shutdown();
 
         virtual Rectangle GetRect();
 
-    private:
-        virtual void InitResource() {};
-        virtual void InitPhysics(Vector2 pos, b2WorldId world_id) {
-             m_body_def = b2DefaultBodyDef();
-            m_body_def.position = { pos.x, pos.y }; // Different type, raylib Vector2 and box2d vec
-            m_body_id = b2CreateBody(world_id, &m_body_def);
-        };
+        void AddNewPlugin(std::unique_ptr<Plugin> p);
+
     private:
         b2BodyDef m_body_def;
         b2BodyId m_body_id;
+
+        std::vector<Plugin> m_plugins;
 
     protected:
         Rectangle m_rect;
