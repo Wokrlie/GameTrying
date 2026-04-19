9dfc3efd72e2733b673e905a2b29ad82738b8997
Wokrlie <hehelilibz@gmail.com>
2026-04-19 14:07:54 +0800

    add useful docs to `plugin.h`

--- src/plugin.h
+++ src/plugin.h
@@ -5,4 +5,10 @@ class Plugin {
         virtual void Init();
         virtual void Update(float dt);
         virtual void Shutdown();
+
+        /* The plugins should setup by `SetXXX` functions. For example,
+         * ```cpp
+         * Render_plug.SetShader(...); // We are not have it. 
+         * ```
+         */
 }
