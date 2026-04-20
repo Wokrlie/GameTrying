db3f39d14b4dc1351c43cc5f08663b8f304ea922
Wokrlie <hehelilibz@gmail.com>
2026-04-20 18:13:36 +0800

    Add basic shape logic

--- src/plugins/collision.cpp
+++ src/plugins/collision.cpp
@@ -4,6 +4,13 @@ void CollisionBox_plug::Init(b2Vec2 pos, b2Vec2 size, b2WorldId world_id) {
     b2BodyDef body_def = b2DefaultBodyDef();
     body_def.position = pos;
     m_body_id = b2CreateBody(world_id, &body_def);
+
+    b2Polygon box = b2MakeBox(size.x / 2.0f, size.y / 2.0f);
+
+    b2ShapeDef shape_def = b2DefaultShapeDef();
+    shape_def.enableContactEvents = true;
+
+    b2CreatePolygonShape(m_body_id, &shape_def, &box);
 }
 
 void CollisionBox_plug::Shutdown() {
